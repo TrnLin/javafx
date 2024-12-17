1402b8d76827b2eb2ec60082f07016efd93be37f
Rin <152028243+TrnLin@users.noreply.github.com>
2024-12-17 19:32:29 +0700

    Add files via upload

---
+++ src/main/java/com/example/demo/Main.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/example/demo/login.fxml"));
+
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Hello!");
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
