1c7898ce990167482d1d84d427c83d89919475a8
TrnLin <s404097@rmit.edu.vn>
2025-01-11 10:22:47 +0700

    Add rental agreement components and FXML files for viewing and adding rental agreements; remove deprecated visitor.fxml

---
+++ src/main/java/com/example/demo/RentalAgreement/AddRentalAgreementBtn.java
@@ -0,0 +1,4 @@
+package com.example.demo.RentalAgreement;
+
+public class AddRentalAgreementBtn {
+}
---
+++ src/main/java/com/example/demo/RentalAgreement/RentalAgreementContainer.java
@@ -0,0 +1,4 @@
+package com.example.demo.RentalAgreement;
+
+public class RentalAgreementContainer {
+}
